dced9c0bf29184bae5b515a0ee23d8f41bbe8a3e
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-04 04:11:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -30,7 +30,7 @@ class String
     #self.squeeze('.!?').count('.!?')
     #squeeze deletes the doubles of each, then count counts the use of each of these.
     #this does not seem like it would be effective if a sentence ended with ?! but it works for this test
-    string.scan(/[^\.!?]+]\.!?])
+    string.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
     
   end
 end
